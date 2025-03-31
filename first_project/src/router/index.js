74855e48c8856494ea39bf4a9239b58f6a9f1863
0318lyj <0318lyj@gmail.com>
2025-03-31 17:29:37 +0900

    3월 31일 오후 5시 29분

---
+++ first_project/src/router/index.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+//컴포넌트 추가 2) 페이지 단위로 호출
+import DataBinding from '@/views/DataBinding.vue'
+import ListBinding from '@/views/ListBinding.vue'
+
+const routes = [
+  {
+    path: '/', //접속경로
+    name: 'home', //라우트 이름
+    component: HomeView //출력할 컴포넌트
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path : "/dataBinding",
+    name : "dataBind",
+    component: DataBinding,
+  },
+  {
+    path:'/ListBinding',
+    name: 'listBind',
+    component : ListBinding
+  }
+];
+
+// 뷰 라우터를 생성하는 함수
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ first_project/src/views/IfBinding.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <input type="number" v-model="grade">
+    <p v-if="grade >= 90">성적 : A</p>
+    <p v-else>성적 : D</p>
+  </div>
+</template>
+<script>
+
+</script>
